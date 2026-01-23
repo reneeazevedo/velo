5d6805c829814e9a6a35962b3872a4954b5d70e7
reneeazevedo <renee.moura@hotmail.com>
2026-01-22 21:55:37 -0300

    AAA -Arrange, Act, Assert

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -1,19 +1,21 @@
 import { test, expect } from '@playwright/test';
 
-test('deve consultar um pedido aprovado', async ({ page }) => {
-  await page.goto('http://localhost:5173/');
-  // Checkpoint
-  await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint');
-
-  await page.getByRole('link', { name: 'Consultar Pedido' }).click();
-
-  // Checkpoint
+// AAA - Arrange, Act, Assert
+// PAV - Preparar, Agir, Verificar
+test('deve consultar um pedido aprovado', async ({ page }) => { 
+  // Arrange
+  await page.goto('http://localhost:5173/'); 
+  await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint'); 
+  await page.getByRole('link', { name: 'Consultar Pedido' }).click(); 
   await expect(page.getByRole('heading')).toContainText('Consultar Pedido');
-
+  
+  // Act
   await page.getByTestId('search-order-id').fill('VLO-SUG3M1');
   await page.getByTestId('search-order-button').click();
 
-  // Checkpoint
+  // Assert
+  await expect(page.getByTestId('order-result-id')).toBeVisible();
   await expect(page.getByTestId('order-result-id')).toContainText('VLO-SUG3M1');
+  await expect(page.getByTestId('order-result-status')).toBeVisible();
   await expect(page.getByTestId('order-result-status')).toContainText('APROVADO');
 });
