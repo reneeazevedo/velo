06da7775023714c24ca37402aef2418f1e90b119
reneeazevedo <renee.moura@hotmail.com>
2026-02-13 20:03:11 -0300

    Aulas: Um Fluxo, Dois Resultados-Como Parei de Esperar o Dev: IA no Processo de Testes,Testei Antes de Existir: TDD na Vida Real,O ChatGPT agora é um SDET?

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -16,10 +16,21 @@ test.describe('Consulta de Pedido', () => {
 
   test('deve consultar um pedido aprovado', async ({ page }) => {
     //test Data
-    const order = 'VLO-SUG3M1'
+    //const order = 'VLO-SUG3M1'
+    const order = {
+      number: 'VLO-SUG3M1',
+      color: 'Lunar White',
+      status: 'APROVADO',
+      wheels: 'sport Wheels',
+      customer: {
+        name: 'Renee Azevedo',
+        email: 'renee.moura@hotmail.com',
+      },
+      payment: 'À Vista'
+    }
 
     // Act
-    await page.getByRole('textbox', { name: 'Número do Pedido' }).fill(order);
+    await page.getByRole('textbox', { name: 'Número do Pedido' }).fill(order.number);
     await page.getByRole('button', { name: 'Buscar Pedido' }).click();
 
     // Assert
@@ -31,48 +42,201 @@ test.describe('Consulta de Pedido', () => {
     //   .locator('..')//Sobre para o elemento pai (a div que agrupa ambas)
 
     // await expect(containerPedido).toContainText(order, { timeout: 10_000 })
-    await expect(page.getByTestId(`order-result-${order}`)).toMatchAriaSnapshot(`
+    await expect(page.getByTestId(`order-result-${order.number}`)).toMatchAriaSnapshot(`
       - img
       - paragraph: Pedido
-      - paragraph: ${order} 
-      - img
-      - text: APROVADO
+      - paragraph: ${order.number} 
+      - status: 
+        - img
+        - text: ${order.status}
       - img "Velô Sprint"
       - paragraph: Modelo
       - paragraph: Velô Sprint
       - paragraph: Cor
-      - paragraph: Lunar White
+      - paragraph: ${order.color}
       - paragraph: Interior
       - paragraph: cream
       - paragraph: Rodas
-      - paragraph: sport Wheels
+      - paragraph: ${order.wheels}
       - heading "Dados do Cliente" [level=4]
       - paragraph: Nome
-      - paragraph: Renee Azevedo
+      - paragraph: ${order.customer.name}
       - paragraph: Email
-      - paragraph: renee.moura@hotmail.com
+      - paragraph: ${order.customer.email}
       - paragraph: Loja de Retirada
       - paragraph
       - paragraph: Data do Pedido
       - paragraph: /\\d+\\/\\d+\\/\\d+/
       - heading "Pagamento" [level=4]
-      - paragraph: À Vista
+      - paragraph: ${order.payment}
       - paragraph: /R\\$ \\d+\\.\\d+,\\d+/
       `, { timeout: 10_000 });
+      const statusBadge = page.getByRole('status').filter({ hasText: order.status })
+      await expect(statusBadge).toHaveClass(/bg-green-100/)
+      await expect(statusBadge).toHaveClass(/text-green-700/)
+
+      const statusIcon = statusBadge.locator('svg')
+      await expect(statusIcon).toHaveClass(/lucide-circle-check-big/)
   });
 
-  test('deve exibir mensagem quando o pedido não é encontrado', async ({ page }) => {
+  test('deve consultar um pedido reprovado', async ({ page }) => {
+    //test Data
+    //const order = 'VLO-SUG3M1'
+    const order = {
+      number: 'VLO-3NO30X',
+      color: 'Midnight Black',
+      status: 'REPROVADO',
+      wheels: 'sport Wheels',
+      customer: {
+        name: ' Steve Jobs',
+        email: 'steve@apple.com',
+      },
+      payment: 'À Vista'
+    }
 
-    const order = generateOrderCode()
-
-    await page.getByRole('textbox', { name: 'Número do Pedido' }).fill(order);
+    // Act
+    await page.getByRole('textbox', { name: 'Número do Pedido' }).fill(order.number);
     await page.getByRole('button', { name: 'Buscar Pedido' }).click();
 
-    await expect(page.locator('#root')).toMatchAriaSnapshot(`
+    // Assert
+    // const orderCode = page.locator('//p[text()="Pedido"]/../p[text()="VLO-SUG3M1"]')
+    // await expect(orderCode).toBeVisible({ timeout: 10_000 });
+
+    // const containerPedido = page.getByRole('paragraph')
+    //   .filter({ hasText: /^Pedido$/ })
+    //   .locator('..')//Sobre para o elemento pai (a div que agrupa ambas)
+
+    // await expect(containerPedido).toContainText(order, { timeout: 10_000 })
+    await expect(page.getByTestId(`order-result-${order.number}`)).toMatchAriaSnapshot(`
+      - img
+      - paragraph: Pedido
+      - paragraph: ${order.number} 
+      - status: 
+        - img
+        - text: ${order.status}
+      - img "Velô Sprint"
+      - paragraph: Modelo
+      - paragraph: Velô Sprint
+      - paragraph: Cor
+      - paragraph: ${order.color}
+      - paragraph: Interior
+      - paragraph: cream
+      - paragraph: Rodas
+      - paragraph: ${order.wheels}
+      - heading "Dados do Cliente" [level=4]
+      - paragraph: Nome
+      - paragraph: ${order.customer.name}
+      - paragraph: Email
+      - paragraph: ${order.customer.email}
+      - paragraph: Loja de Retirada
+      - paragraph
+      - paragraph: Data do Pedido
+      - paragraph: /\\d+\\/\\d+\\/\\d+/
+      - heading "Pagamento" [level=4]
+      - paragraph: ${order.payment}
+      - paragraph: /R\\$ \\d+\\.\\d+,\\d+/
+      `, { timeout: 10_000 });
+
+      const statusBadge = page.getByRole('status').filter({ hasText: order.status })
+      await expect(statusBadge).toHaveClass(/bg-red-100/)
+      await expect(statusBadge).toHaveClass(/text-red-700/)
+
+      const statusIcon = statusBadge.locator('svg')
+      await expect(statusIcon).toHaveClass(/lucide-circle-x/)
+  });
+  test('deve consultar um pedido em análise', async ({ page }) => {
+    //test Data
+    //const order = 'VLO-SUG3M1'
+    const order = {
+      number: 'VLO-4ZO0NW',
+      color: 'Lunar White',
+      status: 'EM_ANALISE',
+      wheels: 'aero Wheels',
+      customer: {
+        name: ' João Silva',
+        email: 'joao@mock.com',
+      },
+      payment: 'À Vista'
+    }
+
+    // Act
+    await page.getByRole('textbox', { name: 'Número do Pedido' }).fill(order.number);
+    await page.getByRole('button', { name: 'Buscar Pedido' }).click();
+
+    // Assert
+    // const orderCode = page.locator('//p[text()="Pedido"]/../p[text()="VLO-SUG3M1"]')
+    // await expect(orderCode).toBeVisible({ timeout: 10_000 });
+
+    // const containerPedido = page.getByRole('paragraph')
+    //   .filter({ hasText: /^Pedido$/ })
+    //   .locator('..')//Sobre para o elemento pai (a div que agrupa ambas)
+
+    // await expect(containerPedido).toContainText(order, { timeout: 10_000 })
+    await expect(page.getByTestId(`order-result-${order.number}`)).toMatchAriaSnapshot(`
+      - img
+      - paragraph: Pedido
+      - paragraph: ${order.number} 
+      - status: 
+        - img
+        - text: ${order.status}
+      - img "Velô Sprint"
+      - paragraph: Modelo
+      - paragraph: Velô Sprint
+      - paragraph: Cor
+      - paragraph: ${order.color}
+      - paragraph: Interior
+      - paragraph: cream
+      - paragraph: Rodas
+      - paragraph: ${order.wheels}
+      - heading "Dados do Cliente" [level=4]
+      - paragraph: Nome
+      - paragraph: ${order.customer.name}
+      - paragraph: Email
+      - paragraph: ${order.customer.email}
+      - paragraph: Loja de Retirada
+      - paragraph
+      - paragraph: Data do Pedido
+      - paragraph: /\\d+\\/\\d+\\/\\d+/
+      - heading "Pagamento" [level=4]
+      - paragraph: ${order.payment}
+      - paragraph: /R\\$ \\d+\\.\\d+,\\d+/
+      `, { timeout: 10_000 });
+
+      const statusBadge = page.getByRole('status').filter({ hasText: order.status })
+      await expect(statusBadge).toHaveClass(/bg-amber-100/)
+      await expect(statusBadge).toHaveClass(/text-amber-700/)
+
+      const statusIcon = statusBadge.locator('svg')
+      await expect(statusIcon).toHaveClass(/lucide-clock/)
+  });
+
+
+  
+  test.describe('Consulta de Pedido', () => {
+
+    test.beforeEach(async ({ page }) => {
+      // Arrange
+      await page.goto('http://localhost:5173/');
+      await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint');
+
+      await page.getByRole('link', { name: 'Consultar Pedido' }).click();
+      await expect(page.getByRole('heading')).toContainText('Consultar Pedido');
+    });
+
+
+    test('deve exibir mensagem quando o pedido não é encontrado', async ({ page }) => {
+
+      const order = generateOrderCode()
+
+      await page.getByRole('textbox', { name: 'Número do Pedido' }).fill(order);
+      await page.getByRole('button', { name: 'Buscar Pedido' }).click();
+
+      await expect(page.locator('#root')).toMatchAriaSnapshot(`
       - img
       - heading "Pedido não encontrado" [level=3]
       - paragraph: Verifique o número do pedido e tente novamente
       `);
 
-  })
+    })
+  });
 })
