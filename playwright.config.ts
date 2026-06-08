37ed715968481b11139d523d9c95d73e5fe19916
reneeazevedo <renee.moura@hotmail.com>
2026-06-08 10:18:44 -0300

    Desafio Final

--- playwright.config.ts
+++ playwright.config.ts
@@ -43,7 +43,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'https://velo-fjhr4cai3-renee-azevedo-s-projects.vercel.app',
+    baseURL: process.env.BASE_URL || 'http://localhost:5173',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on',
@@ -96,9 +96,9 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  webServer: {
-    command: 'yarn dev',
-    url: 'http://localhost:5173',
-    reuseExistingServer: !process.env.CI,
-  },
+  //webServer: {
+   // command: 'yarn dev',
+   // url: 'http://localhost:5173',
+   // reuseExistingServer: !process.env.CI,
+  //},
 });
--- playwright/support/database/database.ts
+++ playwright/support/database/database.ts
@@ -3,16 +3,16 @@ import pg from 'pg'
 import { Kysely, PostgresDialect } from 'kysely'
 import { Database } from './schema'
 
-const isSupabase = process.env.DATABASE_URL?.includes('supabase.com')
-
-const pool = new pg.Pool({
-  connectionString: process.env.DATABASE_URL,
-  max: 10,
-  ssl: isSupabase ? { rejectUnauthorized: false } : undefined,
+const dialect = new PostgresDialect({
+  pool: new pg.Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+    max: 10,
+  })
 })
 
 export const db = new Kysely<Database>({
-  dialect: new PostgresDialect({
-    pool: pool as ConstructorParameters<typeof PostgresDialect>[0]['pool'],
-  }),
+  dialect,
 })
